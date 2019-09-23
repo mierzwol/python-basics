4b643ff616902b18ccf8cad5cd2a3ecfbe5b5fda
mierzwol <wlozmitam@gmail.com>
2019-09-23 14:53:07 +0200

    zadanie nr1

---
+++ zadania python/01-python_basics.py
@@ -0,0 +1,24 @@
+print("This is a line of text!")
+
+print("This is first line of text!\nThis is second line!")
+
+# \ Prints out the " that normally would indicate the end of string
+print("This is some \" text")
+
+phrase = "String in a variable"
+print(phrase)
+
+print("\n" + phrase + " and concatenated string")
+
+print(phrase.capitalize())
+print(phrase.upper())
+x=phrase.upper()
+print(phrase.isupper())
+print(x)
+print(x.isupper())
+
+print(len(phrase))
+print(phrase[2])
+print(phrase.index("S"))
+print(phrase.index("i"))
+print(phrase.replace("variable","variable_replaced"))
